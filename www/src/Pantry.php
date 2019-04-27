ab82ab710f22a72f23813b6ed6c36205c0a36d23
realityfabric <realityfabric@gmail.com>
2019-04-27 11:20:29 -0700

    Add Pantry class

---
+++ www/src/Pantry.php
@@ -0,0 +1,29 @@
+<?php
+class Pantry
+{
+	private ingredients; // array of Ingredient objects\
+
+	/* hasIngredient
+	 * Checks if the pantry has a specific ingredient.
+	 * @ingredient The ingredient in question
+	 */
+	public function hasIngredient ($ingredient) {
+		// TODO: check for ingredient
+	}
+
+	/* addIngredient
+	 * Adds an ingredient to the User's pantry
+	 * @ingredient The ingredient being added
+	 */
+	public function addIngredient ($ingredient) {
+		// TODO: add ingredient
+	}
+
+	/* removeIngredient
+	 * Removes an ingredient from the User's pantry
+	 * @ingredient The ingredient being removed.
+	 */
+	public function removeIngredient ($ingredient) {
+		// TODO: remove ingredient
+	}
+}
