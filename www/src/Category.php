9d00d3fbb72e74fe403a224e2881fa46286f3b0a
realityfabric <realityfabric@gmail.com>
2019-04-27 11:17:01 -0700

    Add Category class

---
+++ www/src/Category.php
@@ -0,0 +1,20 @@
+<?php
+class Category
+{
+	private $id; // int
+	private $name; // string
+
+	/* getName
+	 * Returns the name of the category
+	 */
+	public function getName () {
+		// TODO: get name
+	}
+
+	/* getId
+	 * Returns the id of the category
+	 */
+	public function getId () {
+		// TODO: get id
+	}
+}
