0d8fc2f01f1f353b6bc5f17036f89da558f02a5f
Jessica Fairchild <realityfabric@gmail.com>
2019-04-30 11:14:52 -0700

    Implement existing Recipe class methods

--- www/src/Recipe.php
+++ www/src/Recipe.php
@@ -9,37 +9,42 @@ class Recipe
 
 	/* getName
 	 * Returns the name of the Recipe
+	 * @return The name of the Recipe instance
 	 */
 	public function getName () {
-		// TODO: return name
+		return $this->name;
 	}
 
 	/* getId
 	 * Returns the id of the Recipe
+	 * @return The ID of the Recipe instance
 	 */
 	public function getId () {
-		// TODO return id
+		return $this->id;
 	}
 
 	/* getCategories
 	 * Returns an array of categories which the Recipe is associated with
+	 * @return The array of Category objects associated with the Recipe instance
 	 */
 	public function getCategories () {
-		// TODO: return array of categories
+		return $this->categories;
 	}
 
 	/* getIngredients
 	 * Returns an array of tuples which consist of ingredients and their quantities, as required by the recipe
+	 * @return The array of tuples of Ingredient objects and Quantities.
 	 */
 	public function getIngredients () {
-		// TODO: get ingredients and quantities
+		return $this->ingredients;
 	}
 
 	/* getInstructions
 	 * Returns the recipe's instructions
+	 * @return The instructions for the Recipe instance
 	 */
 	public function getInstructions () {
-		// TODO: get instructions
+		return $this->instructions;
 	}
 
 	/* hasCategory
@@ -48,6 +53,11 @@ class Recipe
 	 * @flag a boolean value: true if category is associated with the Recipe, otherwise false
 	 */
 	public function hasCategory ($category) {
-		// TODO: check for category
+		// TODO: input validation
+		foreach ($this->categories as $c) {
+			if ($c == $category)
+				return true;
+			}
+		return false;
 	}
 }
