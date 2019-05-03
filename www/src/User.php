a5d220a54a8d9d47605ec1deb7036c08c624c619
readychef <realityfabric@gmail.com>
2019-05-02 17:10:40 -0700

    Partially implement login function
    Currently only returns true if credentials match the account table or false if no records match.
    Other things need to be implemented before this can be completed.

--- www/src/User.php
+++ www/src/User.php
@@ -1,4 +1,6 @@
 <?php
+include_once("../config/config.php");
+include_once("../src/Pantry.php");
 /*
  * This class stores user data, including a pointer to an instance of the Pantry class specific to the user.
  * User data is pulled from the user table.
@@ -16,14 +18,34 @@ class User
 	public function getPantry () {
 		// TODO
 	}
-	
+
 	/* login
 	 * Generates an instance of a User object for a user
 	 * @username the username of the user
 	 * @password the password of the user
-	 * @user the instance of the User class
+	 * @return the instance of the User class, or false if failed
 	 */
 	public function login ($username, $password) {
-		// TODO
+		global $db;
+
+		$dbhost = $db['host'];
+		$dbuser = $db['user'];
+		$dbpassword = $db['password'];
+
+		$dbconn = pg_connect("host='localhost' user='readychef' password='ReadyChefDev'");
+
+		$sanitize_username = pg_escape_string($username);
+		$sanitize_password = pg_escape_string($password);
+
+		$result = pg_query($dbconn, "SELECT * FROM account WHERE username = '$sanitize_username'");
+		$account = pg_fetch_assoc($result);
+
+		if (password_verify($password, $account['hash'])) {
+			// TODO: create instance of Pantry associated with User
+			// TODO: create instance of User
+			return true; // TODO: return instance of User
+		} else {
+			return false;
+		}
 	}
 }
