503ab329c11f0b2f6746f6e0b392b178f12a2fd4
realityfabric <realityfabric@gmail.com>
2019-05-11 10:26:07 -0700

    Add index (home) page

---
+++ www/public/index.php
@@ -0,0 +1,14 @@
+<?php
+include_once("../config/config.php");
+include_once("../src/User.php");
+session_start()
+?>
+<!doctype html>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<?php include("../includes/header.php"); ?>
+<p>Welcome to ReadyChef<?php if(isset($_SESSION['user'])) echo ", " . $_SESSION['user']->getUsername(); ?>.</p>
+<?php include("../includes/footer.php"); ?>
+</html>
