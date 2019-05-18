8977f4a2c7cedf70497b643dae29bdba55a8495a
realityfabric <realityfabric@gmail.com>
2019-05-18 15:26:05 -0700

    Update navigation to include logged in options

--- www/includes/navigation.php
+++ www/includes/navigation.php
@@ -1,6 +1,17 @@
+<?php
+session_start();
+?>
 <nav>
 <a href="index.php">Home</a>
 <a href="login.php">Log In</a>
 <a href="logout.php">Log Out</a>
 <a href="register.php">Register</a>
+<?php
+if (isset($_SESSION['user'])) {
+?>
+<a href="profile.php">My Pantry</a>
+<a href="search.php">Recipe Search</a>
+<?php
+}
+?>
 </nav>
